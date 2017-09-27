518cf704ec38584470b44a7c84a911461ba62c50
simonguozirui <simonguozirui@gmail.com>
2017-09-27 09:13:05 -0400

    Initial Commit

---
+++ HBDSaraTests/HBDSaraTests.swift
@@ -0,0 +1,36 @@
+//
+//  HBDSaraTests.swift
+//  HBDSaraTests
+//
+//  Created by Simon Guo on 2017-09-27.
+//  Copyright © 2017 Simon Guo. All rights reserved.
+//
+
+import XCTest
+@testable import HBDSara
+
+class HBDSaraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
